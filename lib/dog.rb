9e2c54366047cc97717eb4b49789313fac902a44
Joanna <astronautnemo@gmail.com>
2023-02-08 16:54:20 +0300

    completed code-along

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    # Setter method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    # Getter method
+    def name
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+# Calling setter Dog.name=
+lassie.name = "Lassie"
+# Calling getter Dog.name
+puts lassie.name
